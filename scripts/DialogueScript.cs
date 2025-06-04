1001ff83532292c52540291549d0019be43bd893
Harkipu <clifftoven@gmail.com>
2025-06-04 21:54:22 +0700

    More Dialogue

--- scripts/DialogueScript.cs
+++ scripts/DialogueScript.cs
@@ -4,39 +4,67 @@ using UnityEngine;
 using TMPro;
 using UnityEditor;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 
 
 public class DialogueScript : MonoBehaviour
 {
-    public TMP_Text TextBoxComponent; 
-    public string[] ShelterLines = {"Excuse me... Can I please have shelter for the night?"};
-    public string[] FoodLines = {"I'm starving... Can I please have some food?"};
+    public TMP_Text TextBoxComponent;
+    public string[] ShelterLines1 = { "Are there any beds available left?" };
+    public string[] ShelterLines2 = { "Can I stay here for the night?" };
+    public string[] ShelterLines3 = { "Is there any spot that has not been taken yet?" };
+    public string[] ShelterLines4 = { "Can I get a bed to sleep on here?" };
+    public string[] ShelterLines5 = { "May I rest here for the night?" };
+
+    public string[] SoupLines1 = { "Do you have any soup left?" };
+    public string[] SoupLines2 = { "I could eat something hot right now..." };
+    public string[] SoupLines3 = { "May I have some soup?" };
+    public string[] SoupLines4 = { "I have been feeling a bit sick these past few days, do you have that can help with that?" };
+    public string[] SoupLines5 = { "I'll take the soup if you have any left." };
+
+    public string[] PorridgeLines1 = { "Is there any porridge left?" };
+    public string[] PorridgeLines2 = { "Do you have anything that is easy to digest?" };
+    public string[] PorridgeLines3 = { "You have porridges right? i'll take one." };
+    public string[] PorridgeLines4 = { "A porridge sounds nice to eat right now." };
+    public string[] PorridgeLines5 = { "I'll take a bowl of porridge please." };
+
+    public string[] SandwichLines1 = { "Can I get a sandwich please?" };
+    public string[] SandwichLines2 = { "Are there any sandwiches left?" };
+    public string[] SandwichLines3 = { "I'll take a sandwich." };
+    public string[] SandwichLines4 = { "Bread good, have any?" };
+    public string[] SandwichLines5 = { "That sandwich looks nice." };
 
     [HideInInspector]
     public string[] Lines;
     public float TextSpeed;
     private int index;
     private Coroutine TypingCoroutine;
+    public GameObject ChoicePanel;
+    public Button AllowButton, RejectButton;
+    public bool ReadyForShelterchoice = false;
 
     // Start is called before the first frame update
     void Start()
     {
         TextBoxComponent.text = string.Empty;
         gameObject.SetActive(false);
+
+        AllowButton.onClick.AddListener(HandleAllowShelter);
+        RejectButton.onClick.AddListener(HandleRejectShelter);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if(TextBoxComponent.text == Lines[index])
+            if (TextBoxComponent.text == Lines[index])
             {
                 NextLine();
             }
             else
             {
-                if(TypingCoroutine != null)
+                if (TypingCoroutine != null)
                 {
                     StopCoroutine(TypingCoroutine);
                 }
@@ -54,7 +82,7 @@ public class DialogueScript : MonoBehaviour
     IEnumerator TypeLine()
     {
         TextBoxComponent.text = "";
-        foreach(char c in Lines[index].ToCharArray())
+        foreach (char c in Lines[index].ToCharArray())
         {
             TextBoxComponent.text += c;
             yield return new WaitForSeconds(TextSpeed);
@@ -62,11 +90,11 @@ public class DialogueScript : MonoBehaviour
     }
     public void NextLine()
     {
-        if(index<Lines.Length - 1)
+        if (index < Lines.Length - 1)
         {
             index++;
             TextBoxComponent.text = "";
-            if(TypingCoroutine != null)
+            if (TypingCoroutine != null)
             {
                 StopCoroutine(TypingCoroutine);
             }
@@ -74,8 +102,36 @@ public class DialogueScript : MonoBehaviour
         }
         else
         {
-            gameObject.SetActive(false);
+            if (Lines == ShelterLines1 || Lines == ShelterLines2 || Lines == ShelterLines3 || Lines == ShelterLines4 || Lines == ShelterLines5)
+            {
+                ReadyForShelterchoice = true;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 
+    public void ShowShelterChoices()
+    {
+        ChoicePanel.SetActive(true);
+    }
+
+    void HandleAllowShelter()
+    {
+        Debug.Log("Shelter Granted.");
+        ChoicePanel.SetActive(false);
+        gameObject.SetActive(false);
+        ReadyForShelterchoice = false;
+    }
+
+    void HandleRejectShelter()
+    {
+        Debug.Log("Redirect to another shelter.");
+        ChoicePanel.SetActive(false);
+        gameObject.SetActive(false);
+        ReadyForShelterchoice = false;
+    }
 }
--- scripts/NPCMovement.cs
+++ scripts/NPCMovement.cs
@@ -24,14 +24,89 @@ public class NPCMovement : MonoBehaviour
         if(!TriggerDialogue && Vector3.Distance(transform.position, targetPosition) < 0.1f)
         {
             TriggerDialogue = true;
-            int choice = Random.Range(0, 2);
+            int choice = Random.Range(0, 20);
             if(choice == 0)
             {
-                dialogue.Lines = dialogue.ShelterLines;
+                dialogue.Lines = dialogue.ShelterLines1;
             }
-            else
+            else if(choice == 1)
             {
-                dialogue.Lines = dialogue.FoodLines;
+                dialogue.Lines = dialogue.ShelterLines2;
+            }
+            else if(choice == 2)
+            {
+                dialogue.Lines = dialogue.ShelterLines3;
+            }
+            else if(choice == 3)
+            {
+                dialogue.Lines = dialogue.ShelterLines4;
+            }
+            else if(choice == 4)
+            {
+                dialogue.Lines = dialogue.ShelterLines5;
+            }
+
+            else if(choice == 5)
+            {
+                dialogue.Lines = dialogue.SoupLines1;
+            }
+            else if(choice == 6)
+            {
+                dialogue.Lines = dialogue.SoupLines2;
+            }
+            else if(choice == 7)
+            {
+                dialogue.Lines = dialogue.SoupLines3;
+            }
+            else if(choice == 8)
+            {
+                dialogue.Lines = dialogue.SoupLines4;
+            }
+            else if(choice == 9)
+            {
+                dialogue.Lines = dialogue.SoupLines5;
+            }
+
+            else if(choice == 10)
+            {
+                dialogue.Lines = dialogue.PorridgeLines1;
+            }
+            else if(choice == 11)
+            {
+                dialogue.Lines = dialogue.PorridgeLines2;
+            }
+            else if(choice == 12)
+            {
+                dialogue.Lines = dialogue.PorridgeLines3;
+            }
+            else if(choice == 13)
+            {
+                dialogue.Lines = dialogue.PorridgeLines4;
+            }
+            else if(choice == 14)
+            {
+                dialogue.Lines = dialogue.PorridgeLines5;
+            }
+
+            else if(choice == 15)
+            {
+                dialogue.Lines = dialogue.SandwichLines1;
+            }
+            else if(choice == 16)
+            {
+                dialogue.Lines = dialogue.SandwichLines2;
+            }
+            else if(choice == 17)
+            {
+                dialogue.Lines = dialogue.SandwichLines3;
+            }
+            else if(choice == 18)
+            {
+                dialogue.Lines = dialogue.SandwichLines4;
+            }
+            else if(choice == 19)
+            {
+                dialogue.Lines = dialogue.SandwichLines5;
             }
             dialogue.gameObject.SetActive(true);
             dialogue.StartDialogue();
